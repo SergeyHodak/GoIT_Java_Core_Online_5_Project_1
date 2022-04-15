30f67ed1b596cf7e75d2ca6677640bd1f2be5e8f
1488Max <bargamin123@gmail.com>
2022-04-15 20:14:37 +0300

    .

--- src/main/java/body/response/DecimalPlaces.java
+++ src/main/java/body/response/DecimalPlaces.java
@@ -1,11 +1,16 @@
 package body.response;
 
+import body.FSM.ChatSettings;
+
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class DecimalPlaces {
-    public static float rounding(int decimalPlaces,float value){
+
+    public static float rounding(float value){
         BigDecimal result = new BigDecimal(value);
+        ChatSettings chatSettings = new ChatSettings();
+        int decimalPlaces = chatSettings.getQuantityOfSignsAfterDot();
         result = result.setScale(decimalPlaces, RoundingMode.UP);
         return result.floatValue();
     }
