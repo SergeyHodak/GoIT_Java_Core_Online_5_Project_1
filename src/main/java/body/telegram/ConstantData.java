87ec9d31590a04e6f0793767d49274c722a61d1f
SergeyHodak <87858809+SergeyHodak@users.noreply.github.com>
2022-04-20 22:28:19 +0300

    initialization

--- src/main/java/body/telegram/ConstantData.java
+++ src/main/java/body/telegram/ConstantData.java
@@ -3,5 +3,4 @@ package body.telegram;
 public class ConstantData {
     public static final String BOT_NAME = "SpyForExchangeRates_bot";
     public static final String BOT_TOKEN = "5238569176:AAEBjX4NtWzgk4VyF8Jmp4_fsxtLtg_6wyw";
-
 }
